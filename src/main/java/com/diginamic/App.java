ede96b1cf43cd110c5119a67acef653374b2b442
Jordan F <ferrejordan8@gmail.com>
2018-01-05 15:46:02 +0100

    tp 3 - profils

--- src/main/java/com/diginamic/App.java
+++ src/main/java/com/diginamic/App.java
@@ -1,5 +1,6 @@
 package com.diginamic;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 
@@ -10,7 +11,12 @@ import com.github.lalyos.jfiglet.FigletFont;
 public class App {
 	
 	  public static void main(String[] args) {
-	    String asciiArt = FigletFont.convertOneLine("App");
-	    System.out.println(asciiArt);
+		  String titre = ResourceBundle.getBundle("application").getString("titre");
+		  String asciiArt = FigletFont.convertOneLine(titre);
+		  System.out.println(asciiArt);
+		  
+		  //affichage de l'environnemnt
+		  String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		  System.out.println("Environnement : " + environnement);
 	  }
 	}
