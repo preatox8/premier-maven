1cc9fd5acbadaf173c8329b6a753e893adc3314a
Jordan F <ferrejordan8@gmail.com>
2018-01-05 14:35:13 +0100

    modification etape 7 tp 1

--- src/main/java/com/diginamic/App.java
+++ src/main/java/com/diginamic/App.java
@@ -1,13 +1,16 @@
 package com.diginamic;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+public class App {
+	
+	  public static void main(String[] args) {
+	    String asciiArt = FigletFont.convertOneLine("hello");
+	    System.out.println(asciiArt);
+	  }
+	}
